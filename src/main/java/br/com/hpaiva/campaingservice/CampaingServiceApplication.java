e46db775630bd9e7da62061cd6935810e4bf064d
Herivelton Paiva <hpaiva@uolinc.com>
2020-02-28 22:49:15 -0300

    initial commit

---
+++ src/main/java/br/com/hpaiva/campaingservice/CampaingServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.hpaiva.campaingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hpaiva/campaingservice/CampaingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hpaiva.campaingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
